54b8651d67b4cdf0a591698b734984303d47978c
Keshavnagar5 <keshavnagar2003@gmail.com>
2025-10-05 04:11:03 +0530

    v1

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom'
 import Login from '../screens/Login'
 import Register from '../screens/Register'
 import Home from '../screens/Home'
 import Project from '../screens/Project'
+import { UserContext } from '../context/user.context'
 
 const Approutes = () => {
+  const { user } = useContext(UserContext)
+
   return (
     <BrowserRouter>
-       
-       <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />   
-            <Route path="/project" element={<Project />} />     
-  
-       </Routes>
+      <Routes>
+        {/* Redirect to login if not logged in */}
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" replace />} />
+
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected route */}
+        <Route path="/project" element={user ? <Project /> : <Navigate to="/login" replace />} />
+
+        {/* Catch-all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </BrowserRouter>
-)
+  )
 }
 
 export default Approutes
